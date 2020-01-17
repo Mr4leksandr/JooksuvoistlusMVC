cd25fc46ba46aa7d6081aa1f07dca4aba0b57158
Opilane TTHK <opilane@tthk.ee>
2020-01-17 09:14:52 +0200

    Add project files.

---
+++ JooksuvoistlusMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JooksuvoistlusMVC.Startup))]
+namespace JooksuvoistlusMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
